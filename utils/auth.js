b2446ebeddfba74d9718df40440735d1fa876800
Keana Cobarde <keanacobarde@gmail.com>
2024-04-24 18:20:46 -0500

    setting up auth

--- utils/auth.js
+++ utils/auth.js
@@ -3,22 +3,26 @@ import 'firebase/auth';
 import { clientCredentials } from './client';
 
 const checkUser = (uid) => new Promise((resolve, reject) => {
-  fetch(`${clientCredentials.databaseURL}/checkuser`, {
+  fetch(`${clientCredentials.databaseURL}/api/checkuser`, {
     method: 'POST',
-    body: JSON.stringify({
-      uid,
-    }),
+    body: JSON.stringify({ uid }),
     headers: {
       'Content-Type': 'application/json',
       Accept: 'application/json',
     },
   })
-    .then((resp) => resolve(resp.json()))
+    .then((resp) => {
+      if (resp.ok) {
+        resolve(resp.json());
+      } else {
+        resolve({});
+      }
+    })
     .catch(reject);
 });
 
 const registerUser = (userInfo) => new Promise((resolve, reject) => {
-  fetch(`${clientCredentials.databaseURL}/register`, {
+  fetch(`${clientCredentials.databaseURL}/api/register`, {
     method: 'POST',
     body: JSON.stringify(userInfo),
     headers: {
