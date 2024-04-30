8fd218eba64177b2686981799d7edd16369a7091
Britnay Gore <britnaygore268@gmail.com>
2024-04-29 20:28:27 -0400

    Changed api call to only get user's playlist

--- pages/playlists.js
+++ pages/playlists.js
@@ -1,7 +1,7 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import PlaylistCard from '../components/PlaylistCard';
-import { getAllPlaylists } from '../API/PlaylistData';
+import { getUserPlaylists } from '../API/PlaylistData';
 import { useAuth } from '../utils/context/authContext';
 
 export default function ShowPlaylists() {
@@ -9,19 +9,19 @@ export default function ShowPlaylists() {
 
   const { user } = useAuth();
 
-  const getAllThePlaylists = () => {
-    getAllPlaylists(user.id).then(setPlaylists);
+  const getAllUsersPlaylists = () => {
+    getUserPlaylists(user.id).then(setPlaylists);
   };
 
   useEffect(() => {
-    getAllThePlaylists();
+    getAllUsersPlaylists();
     console.warn(user);
   }, []);
 
   return (
     <div className="d-flex justify-content-center align-items-center">
       {playlists.length === 0 ? <h1 style={{ textAlign: 'center', color: 'white', width: '100%' }}>You Have No Playlists</h1> : playlists.map((playlist) => (
-        <PlaylistCard key={playlist.id} playlistObj={playlist} onUpdate={getAllThePlaylists} />
+        <PlaylistCard key={playlist.id} playlistObj={playlist} onUpdate={getAllUsersPlaylists} />
       ))}
     </div>
   );
