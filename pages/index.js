7aa034aca689bd55e99b797437cb441c4a102a4b
cnfairall <101303134+cnfairall@users.noreply.github.com>
2024-04-29 21:41:44 -0500

    themes

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Button, Card } from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
 
@@ -8,22 +8,17 @@ function Home() {
   return (
     <div
       className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
     >
-      <Card className="console">
-
+      <div className="console audio text-center d-flex flex-column justify-content-center align-content-center">
         <h1>Hello {user.fbUser.displayName}! </h1>
         <p>Your Bio: {user.bio}</p>
         <p>Click the button below to logout!</p>
         <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
           Sign Out
         </Button>
-      </Card>
+      </div>
+      <div className="console-border" />
+
     </div>
   );
 }
