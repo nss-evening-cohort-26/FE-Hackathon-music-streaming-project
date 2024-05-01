ef8b1ba5ab3acb2c011feb1f45488fcf12eb1b77
cnfairall <101303134+cnfairall@users.noreply.github.com>
2024-05-01 18:43:26 -0500

    logo

--- components/NavBar.js
+++ components/NavBar.js
@@ -5,40 +5,36 @@ import Link from 'next/link';
 import { IoMdMenu } from 'react-icons/io';
 
 export default function NavBar() {
-  const [show, setShow] = useState(false);
-  const handleShow = () => {
-    setShow(true);
-  };
-  const handleClose = () => {
-    setShow(false);
+  const [open, setOpen] = useState(false);
+  const toggleOpen = () => {
+    if (open) {
+      setOpen(false);
+    } else {
+      setOpen(true);
+    }
   };
 
   return (
     <div id="menu">
       <Button
-        id="blob"
+        id="toggle"
+        className="xbutton"
         type="button"
-        onClick={handleShow}
-        style={{ margin: '8px' }}
+        onClick={toggleOpen}
+        style={{ zIndex: '20', margin: '8px' }}
       >
-        <IoMdMenu />
+        {!open
+          ? <IoMdMenu />
+          : <span>X</span>}
       </Button>
       <div
         id="nav"
-        className={show ? 'w-60 fixed inset-y-0 z-10 flex flex-col align-items-start' : 'hidden'}
+        className={open ? 'open fixed inset-y-0 z-10 flex flex-col align-items-start' : 'hidden fixed inset-y-0 z-10 flex flex-col align-items-start'}
 
       >
-        <Button
-          id="close"
-          className="xbutton"
-          onClick={handleClose}
-          style={{ zIndex: '20', margin: '8px' }}
-        >
-          <span>X</span>
-        </Button>
         <svg
           id="bar"
-          className={show ? 'absolute inset-0 w-full h-full text-white' : 'hidden'}
+          className="absolute inset-0 w-full h-full text-white"
           style={{ filter: 'drop-shadow(10px 0 10px #2C2C2C)' }}
           preserveAspectRatio="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -52,10 +48,7 @@ export default function NavBar() {
           </defs>
           <path d="M 268.487 0 H 0 V 800 H 247.32 Q 400 735 311 643 Q 120 446 268.487 367.647 Q 593 190 268.487 0 Z" fill="url(#gradient)" stroke="#2C2C2C" strokeWidth="3" strokeOpacity="0.4" />
         </svg>
-        <div className="z-10 flex flex-col flex-1 p-1">
-          <div className="audio flex items-center justify-between flex-shrink-0 w-64 p-2 mt-2">
-            <Link style={{ color: 'black' }} passHref href="/">Y2Play</Link>
-          </div>
+        <div className="z-10 flex flex-col flex-1 p-1 mt-5">
           <div className="audio flex items-center justify-between flex-shrink-0 w-64 p-2">
             <Link style={{ color: 'black' }} passHref href="/">Home</Link>
           </div>
--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,10 @@
 function Home() {
   return (
-    <div
-      className="console-box text-center d-flex flex-column justify-content-center align-content-center"
-    >
-      <div className="console audio">
-        <svg
-          width="700"
-          height="500"
-          viewBox="0 0 1006 723"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-          style={{ filter: 'drop-shadow(10px 0 10px #2C2C2C)' }}
-        >
-          <path d="M43.2817 717.194L872.302 721.568C893.318 721.679 911.181 706.239 914.113 685.427L1003.75 49.1104C1007.51 22.4419 985.502 -0.815671 958.665 1.40155C690.738 23.5369 519.838 38.281 204.984 71.7967C188.111 73.5928 173.911 85.384 169.104 101.661L3.22337 663.298C-4.69844 690.12 15.315 717.047 43.2817 717.194Z" fill="url(#paint0_radial_4_2)" stroke="#2B2C34" strokeWidth=".5" />
-          <defs>
-            <radialGradient id="paint0_radial_4_2" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(566.474 381.83) rotate(98.1449) scale(359.311 522.873)">
-              <stop stopColor="white" />
-              <stop offset="1" stopColor="#787A91" />
-            </radialGradient>
-          </defs>
-        </svg>
+    <>
+      <div className="flex flex-col justify-content-center">
+        <img alt="Y2Play logo" src="./image 1.png" />
       </div>
-      <div className="text-box">
-        <img alt="Y2Play logo" src="./logoclear.png" />
-      </div>
-
-    </div>
+    </>
   );
 }
 
