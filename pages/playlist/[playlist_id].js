efcbd048792c8439e7208a59fd0c5aa13e1e927f
Yarelis Martin <yarelis0martin@gmail.com>
2024-05-01 20:54:30 -0500

    updated the new/old function so it looks cleaner

--- pages/playlist/[playlist_id].js
+++ pages/playlist/[playlist_id].js
@@ -38,9 +38,16 @@ export default function PlaylistDetails() {
     <div style={{ marginLeft: '200px', width: '80%' }}>
       <div style={{ display: 'flex', justifyContent: 'space-around', marginTop: '25px' }}>
         <div>
-          <h3 className="display-5 mb-3"> {playlistDetail.name}</h3>
-          <Link href={`/playlist/edit/${playlist_id}`} passHref><Button variant="success" style={{ borderRadius: '20px', padding: '5px 25px', marginRight: '5px' }}>Edit</Button></Link>
-          <Button variant="danger" style={{ borderRadius: '20px', padding: '5px 25px' }} onClick={deleteThisPlaylist}>Delete</Button>
+          <h3 className="display-5 mb-3" style={{ color: 'white' }}> {playlistDetail.name}</h3>
+
+          <Link href={`/playlist/edit/${playlist_id}`} passHref>
+            <Button variant="success" style={{ borderRadius: '20px', padding: '5px 25px', marginRight: '5px' }}>Edit
+            </Button>
+          </Link>
+
+          <Button variant="danger" style={{ borderRadius: '20px', padding: '5px 25px' }} onClick={deleteThisPlaylist}>Delete
+          </Button>
+
         </div>
         <div style={{ display: 'flex', alignItems: 'flex-end' }}>
           <Button
--- pages/playlists.js
+++ pages/playlists.js
@@ -2,8 +2,10 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import Link from 'next/link';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import PlaylistCard from '../components/PlaylistCard';
-import { getUserPlaylists } from '../api/PlaylistData';
+import { getFavoritePlaylists, getUserPlaylists } from '../api/PlaylistData';
 import { useAuth } from '../utils/context/authContext';
 
 export default function ShowPlaylists() {
@@ -15,6 +17,24 @@ export default function ShowPlaylists() {
     getUserPlaylists(user.id).then(setPlaylists);
   };
 
+  const getFavoritePlaylist = () => {
+    getFavoritePlaylists(user.id).then(setPlaylists);
+    console.warn('fav', getFavoritePlaylists(user.id));
+  };
+
+  const newestAndOldest = (sortBy) => {
+    getUserPlaylists(user.id)
+      .then((userPlaylists) => {
+        let sortedPlaylists;
+        if (sortBy === 'newest') {
+          sortedPlaylists = [...userPlaylists].sort((a, b) => new Date(b.dateCreated) - new Date(a.dateCreated));
+        } else if (sortBy === 'oldest') {
+          sortedPlaylists = [...userPlaylists].sort((a, b) => new Date(a.dateCreated) - new Date(b.dateCreated));
+        }
+        setPlaylists(sortedPlaylists);
+      });
+  };
+
   useEffect(() => {
     getAllUsersPlaylists();
     console.warn(getUserPlaylists(user.id));
@@ -23,6 +43,7 @@ export default function ShowPlaylists() {
   return (
     <div style={{ marginLeft: '100px', width: '80%' }}>
       <div className="text-center"> {/* Wrapper div for centering */}
+
         <Link href="/playlist/new" passHref>
           <Button
             className="info"
@@ -36,6 +57,13 @@ export default function ShowPlaylists() {
             Add A Playlist
           </Button>
         </Link>
+        <DropdownButton id="dropdown-basic-button" title="Filter">
+          <Dropdown.Item href="#/my-playlists" onClick={getAllUsersPlaylists}>All Playlist</Dropdown.Item>
+          <Dropdown.Item href="#/favorite" onClick={getFavoritePlaylist}>Favorite Playlist</Dropdown.Item>
+          <Dropdown.Item href="#/newest" onClick={() => newestAndOldest('newest')}>Newest</Dropdown.Item>
+          <Dropdown.Item href="#/oldest" onClick={() => newestAndOldest('oldest')}>Oldest</Dropdown.Item>
+        </DropdownButton>
+
       </div>
       <div className="d-flex justify-content-center align-items-center flex-wrap">
         {playlists.length === 0 ? <h1 style={{ textAlign: 'center', color: 'white', width: '100%' }}>You Have No Playlists</h1> : playlists.map((playlist) => (
