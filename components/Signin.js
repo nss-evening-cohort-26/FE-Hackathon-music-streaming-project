f1a530e7407c1dc201e79ee3158c4294f49e7636
Yarelis Martin <yarelis0martin@gmail.com>
2024-05-04 16:58:51 -0500

    added styling to sign in

--- components/Signin.js
+++ components/Signin.js
@@ -1,28 +1,38 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { signIn } from '../utils/auth';
 
 function Signin() {
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        margin: '0 auto',
-        zIndex: 1,
-        minHeight: '25rem',
-        width: '100%',
-        minWidth: '30rem',
-        paddingBlock: '0 5rem',
-      }}
-    >
-      <h1>Hi there!</h1>
-      <p>Click the button below to login!</p>
-      <Button type="button" size="lg" className="copy-btn" onClick={signIn}>
-        Sign In
-      </Button>
-    </div>
+    <>
+      <div
+        className="text-center d-flex flex-column justify-content-center align-content-center"
+        style={{
+          height: '90vh',
+          padding: '30px',
+          margin: '0 auto',
+          zIndex: 1,
+          minHeight: '25rem',
+          width: '80%',
+          minWidth: '30rem',
+          paddingBlock: '0 5rem',
+        }}
+      >
+        <div className="row justify-content-center align-items-center">
+          <img
+            style={{
+              zIndex: '6', width: '30em', filter: 'drop-shadow(0 2px 2px white)',
+            }}
+            alt="Y2Play logo"
+            src="./logoclear.png"
+          />
+        </div>
+        <div className="row justify-content-center">
+          <Button style={{ width: '50%' }} className="xbutton audio" onClick={signIn}>Sign In</Button>
+        </div>
+      </div>
+    </>
   );
 }
 
